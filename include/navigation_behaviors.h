37ed60cace1f074e7c17c4d8c6c63cb18ec840e8
pyaesoanaung <romrobotics@gmail.com>
2023-05-12 10:59:48 +0630

    ok

--- include/navigation_behaviors.h
+++ include/navigation_behaviors.h
@@ -1,32 +1,32 @@
-#include "behaviortree_cpp_v3/behavior_tree.h"
 #include "rclcpp/rclcpp.hpp"
+#include "behaviortree_cpp_v3/behavior_tree.h"
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "nav2_msgs/action/navigate_to_pose.hpp"
 
-#include "yaml-cpp/yaml.h"
 #include <tf2/LinearMath/Quaternion.h>
-#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
-#include <string>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 class GoToPose : public BT::StatefulActionNode
 {
 public:
-    GoToPose(const std::string &name, const BT::NodeConfiguration &config, rclcpp::Node::SharedPtr node_ptr);
+  GoToPose(const std::string &name,
+           const BT::NodeConfiguration &config,
+           rclcpp::Node::SharedPtr node_ptr);
 
-    using NavigateToPose = nav2_msgs::action::NavigateToPose;
-    using GoalHandleNav = rclcpp_action::ClientGoalHandle<NavigateToPose>;
+  using NavigateToPose = nav2_msgs::action::NavigateToPose;
+  using GoalHandleNav = rclcpp_action::ClientGoalHandle<NavigateToPose>;
 
-    rclcpp::Node::SharedPtr node_ptr_;
-    rclcpp_action::Client<NavigateToPose>::SharedPtr action_client_ptr_;
-    bool done_flag_;
+  rclcpp::Node::SharedPtr node_ptr_;
+  rclcpp_action::Client<NavigateToPose>::SharedPtr action_client_ptr_;
+  bool done_flag_;
 
-    // Method Overrides
-    static BT::PortsList providePorts();
+  // Method overrides
+  BT::NodeStatus onStart() override;
+  BT::NodeStatus onRunning() override;
+  void onHalted() override{};
 
-    BT::NodeStatus onStart() override;
-    BT::NodeStatus onRunning() override;
-    void onHalted() override{};
+  static BT::PortsList providedPorts();
 
-    // Action client callback
-    void nav_to_pose_callback(const GoalHandleNav::WrappedResult &result);
+  // Action Client callback
+  void nav_to_pose_callback(const GoalHandleNav::WrappedResult &result);
 };
--- src/autonomy_node.cpp
+++ src/autonomy_node.cpp
@@ -1,65 +1,76 @@
-#include "autonomy_node.hpp"
+#include "autonomy_node.h"
 
 using namespace std::chrono_literals;
-const std::string xml_dir = ament_index_cpp::get_package_share_directory("rom2109_autonomy") + "bt_xml";
 
-AutoNode::AutoNode(const std::string &node_name) : Node(node_name)
+const std::string bt_xml_dir =
+    ament_index_cpp::get_package_share_directory("rom2109_autonomy") + "/bt_xml";
+
+AutonomyNode::AutonomyNode(const std::string &nodeName) : Node(nodeName)
 {
-    this->declare_parameter("location_file", "none");
-    RCLCPP_INFO(get_logger(), "Init Done");
+  this->declare_parameter("location_file","none");
+
+  RCLCPP_INFO(get_logger(), "Init done");
 }
 
-void AutoNode::setup()
+void AutonomyNode::setup()
 {
-    // Initial BT setup
-    create_behavior_tree();
-    const auto timer_period = 500ms;
-    timer_ = this->create_wall_timer(timer_period, std::bind(&AutoNode::update_behavior_tree, this));
+  RCLCPP_INFO(get_logger(), "Setting up");
+  create_behavior_tree();
+  RCLCPP_INFO(get_logger(), "BT created");
+
+  const auto timer_period = 500ms;
+  timer_ = this->create_wall_timer(
+      timer_period,
+      std::bind(&AutonomyNode::update_behavior_tree, this));
+
+  rclcpp::spin(shared_from_this());
+  rclcpp::shutdown();
 }
 
-void AutoNode::create_behavior_tree()
+void AutonomyNode::create_behavior_tree()
 {
-    // Create bt
-    BT::BehaviorTreeFactory factory;
+  BT::BehaviorTreeFactory factory;
 
-    // factory.registerNodeType
-    // lambda function
-    // [=] capture external variable form share_from_this()
-    BT::NodeBuilder builder = [=](const std::string &name, const BT::NodeConfiguration &config)
-    {
-        return std::make_unique<GoToPose>(name, config, shared_from_this());
-    }; 
-    factory.registerBuilder<GoToPose>("GoToPose", builder);
+  // register bt node
 
-    tree_ = factory.createTreeFromFile(xml_dir + "/tree.xml");
-    RCLCPP_INFO(get_logger(), "3");
+  BT::NodeBuilder builder =
+      [=](const std::string &name, const BT::NodeConfiguration &config)
+  {
+    return std::make_unique<GoToPose>(name, config, shared_from_this());
+  };
+
+  factory.registerBuilder<GoToPose>("GoToPose", builder);
+
+  RCLCPP_INFO(get_logger(), bt_xml_dir.c_str());
+
+  tree_ = factory.createTreeFromFile(bt_xml_dir + "/tree.xml");
+  RCLCPP_INFO(get_logger(), "3");
 }
 
-void AutoNode::update_behavior_tree()
+void AutonomyNode::update_behavior_tree()
 {
-    BT::NodeStatus tree_status = tree_.tickRoot();
+  BT::NodeStatus tree_status = tree_.tickRoot();
 
-    if (tree_status == BT::NodeStatus::RUNNING)
-    {
-        return;
-    }
-    else if (tree_status == BT::NodeStatus::SUCCESS)
-    {
-        RCLCPP_INFO(this->get_logger(), "Finished Navigation");
-    }
-    else if (tree_status == BT::NodeStatus::FAILURE)
-    {
-        RCLCPP_INFO(this->get_logger(), "Finished Failed");
-        timer_->cancel();
-    }
+  if (tree_status == BT::NodeStatus::RUNNING)
+  {
+    return;
+  }
+  else if (tree_status == BT::NodeStatus::SUCCESS)
+  {
+    RCLCPP_INFO(this->get_logger(), "Finished Navigation");
+  }
+  else if (tree_status == BT::NodeStatus::FAILURE)
+  {
+    RCLCPP_INFO(this->get_logger(), "Navigation Failed");
+    timer_->cancel();
+  }
 }
 
 int main(int argc, char **argv)
 {
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<AutoNode>("autonomy_node");
-    node->setup();
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<AutonomyNode>("autonomy_node");
+  node->setup();
 
-    rclcpp::spin(node);
-    rclcpp::shutdown();
+  return 0;
 }
